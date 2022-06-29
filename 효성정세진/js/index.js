40a8ce851fe5d298ea03c57e404bd1e56f011154
wjdtpwlsrnt3 <70882440+wjdtpwlsrnt3@users.noreply.github.com>
2022-06-29 16:59:56 +0900

    Add files via upload

---
+++ 효성정세진/js/index.js
@@ -0,0 +1,15 @@
+function menuOpen() {
+    document.getElementById('mcontent').style.display = 'block';
+}
+
+function menuClose() {
+    document.getElementById('mcontent').style.display = 'none';
+}
+
+function mediamenuON() {
+    document.getElementById('mediamenu').style.display = 'block';
+}
+
+function mediamenuOFF() {
+    document.getElementById('mediamenu').style.display = 'none';
+}
