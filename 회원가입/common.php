66b2eb5e2782b1032f8c74fbe3836cc897f236fb
wjdtpwlsrnt3 <70882440+wjdtpwlsrnt3@users.noreply.github.com>
2022-07-29 17:32:17 +0900

    Add files via upload

---
+++ 회원가입/common.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+$host = "localhost";
+$user = "root";
+$db   = "hyosung3";
+
+$conn = mysqli_connect($host, $user, null, $db);
+?>
---
+++ 회원가입/sign_up_ok.php
@@ -0,0 +1,41 @@
+<?php 
+
+include('common.php');
+$id = $_POST['id'];
+$password = $_POST['password'];
+$password_check = $_POST['password_check'];
+$name = $_POST['name'];
+$adress = $_POST['adress'];
+$birth = $_POST['birth'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+
+$sql = "insert into sign_up set
+        id = '$id',
+        password = '$password',
+        password_check = '$password_check',
+        name = '$name',
+        adress = '$adress',
+        birth = '$birth',
+        gender = '$gender',
+        email = '$email',
+        phone = '$phone'
+        ";
+
+$result = $conn -> query($sql);
+
+if($result) {
+    echo "
+    <script>
+        location.href='sign_in.php';
+    </script>
+    ";
+} else { 
+    echo "
+    <script>
+        alert('회원가입에 실패했습니다.');
+        history.back();
+    </script>
+    ";
+}
